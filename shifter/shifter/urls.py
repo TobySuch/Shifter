5a5b8c220eb13ab12ac05777db28702f5b573a43
Toby Such <toby@tobysuch.uk>
2022-02-13 17:20:12 +0000

    Add login template

--- shifter/shifter/urls.py
+++ shifter/shifter/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('shifter_auth.urls')),
     path('', include('shifter_files.urls')),
 ]
