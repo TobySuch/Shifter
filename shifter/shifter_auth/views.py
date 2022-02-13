814bba447c48355cc31a4e31d86c191b786f0ec8
Toby Such <toby@tobysuch.uk>
2022-02-13 22:33:23 +0000

    Rework to use built in login view

--- shifter/shifter_auth/views.py
+++ shifter/shifter_auth/views.py
@@ -1,29 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login as login_user
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext as _
-from .forms import LoginForm
+# from django.shortcuts import render
 
-
-def login(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(request, email=form.cleaned_data["email"],
-                                password=form.cleaned_data["password"])
-            if user is not None:
-                login_user(request, user)
-                return redirect("index")
-            else:
-                form.add_error(None, ValidationError(
-                    _("Invalid credentials, please try again."),
-                    code="invalid_credentials"))
-
-    else:
-        form = LoginForm()
-
-    context = {
-        "form": form
-    }
-
-    return render(request, "shifter_auth/login.html", context)
+# Create your views here.
