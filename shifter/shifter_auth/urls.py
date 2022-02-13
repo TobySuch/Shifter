1df2ea73aff0cc910483d58fc5ce58755617aeaf
Toby Such <toby@tobysuch.uk>
2022-02-13 22:58:04 +0000

    Add login link to nav bar

--- shifter/shifter_auth/urls.py
+++ shifter/shifter_auth/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
 
+app_name = "shifter_auth"
 urlpatterns = [
-    path('login', LoginView.as_view(template_name='shifter_auth/login.html',
-                                    redirect_authenticated_user=True)),
+    path('login',
+         LoginView.as_view(template_name='shifter_auth/login.html',
+                           redirect_authenticated_user=True),
+         name="login"),
 ]
