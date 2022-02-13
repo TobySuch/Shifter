2258ea454790e0106b879738ae4c5c39ee9e5a25
Toby Such <toby@tobysuch.uk>
2022-02-13 22:52:23 +0000

    Turn redirect_authenticated_user to true

--- shifter/shifter_auth/urls.py
+++ shifter/shifter_auth/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('login', LoginView.as_view(template_name='shifter_auth/login.html')),
+    path('login', LoginView.as_view(template_name='shifter_auth/login.html',
+                                    redirect_authenticated_user=True)),
 ]
