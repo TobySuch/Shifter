61c0c9be136d57e2854b4c324c097481fc63ec6e
Toby Such <toby@tobysuch.uk>
2022-02-13 22:00:21 +0000

    Add user login logic

--- shifter/shifter_auth/views.py
+++ shifter/shifter_auth/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as login_user
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 from .forms import LoginForm
 
 
 def login(request):
     if request.method == "POST":
-        pass
-    else:
-        context = {
-            "form": LoginForm()
-        }
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, email=form.cleaned_data["email"],
+                                password=form.cleaned_data["password"])
+            if user is not None:
+                login_user(request, user)
+                return redirect("index")
+            else:
+                form.add_error(None, ValidationError(
+                    _("Invalid credentials, please try again."),
+                    code="invalid_credentials"))
 
-        return render(request, "shifter_auth/login.html", context)
+    else:
+        form = LoginForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "shifter_auth/login.html", context)
