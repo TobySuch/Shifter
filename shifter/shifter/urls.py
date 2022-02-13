f5fbb175f0428a601051b664b944311c7166dfd5
Toby Such <toby@tobysuch.uk>
2022-02-13 13:11:10 +0000

    Replace shifter_base with shifter_files app

--- shifter/shifter/urls.py
+++ shifter/shifter/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shifter_base.urls')),
+    path('', include('shifter_files.urls')),
 ]
