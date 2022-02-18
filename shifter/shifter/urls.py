f8aa218c433db376fc2f9a55d522cc9b8a5ec0e2
Toby Such <toby@tobysuch.uk>
2022-02-18 17:26:13 +0000

    Add show more options button

--- shifter/shifter/urls.py
+++ shifter/shifter/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('shifter_auth.urls')),
     path('', include('shifter_files.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
