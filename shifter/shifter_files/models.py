fec6e7118f8392d951ec4b53c3a166541d5f397a
Toby Such <toby@tobysuch.uk>
2022-02-19 14:36:32 +0000

    Add file upload backend

--- shifter/shifter_files/models.py
+++ shifter/shifter_files/models.py
@@ -3,10 +3,12 @@ from django.conf import settings
 
 
 class FileUpload(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.SET_NULL, blank=False,
-                              null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
+                              blank=False, on_delete=models.SET_NULL)
     filename = models.CharField(max_length=255)
     upload_datetime = models.DateTimeField()
     expiary_datetime = models.DateTimeField()
     file_content = models.FileField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.filename
--- shifter/shifter_files/urls.py
+++ shifter/shifter_files/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 
-app_name="shifter_files"
+app_name = "shifter_files"
 urlpatterns = [
     path('', views.FileUploadView.as_view(), name='index'),
 ]
