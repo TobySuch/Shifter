0ce9311cccf1cdcc202c873e4ee9bbe3527877a9
Toby Such <toby@tobysuch.uk>
2022-04-23 22:40:47 +0100

    Add test_force_auth on index test

--- shifter/shifter_files/tests.py
+++ shifter/shifter_files/tests.py
@@ -1,3 +1,13 @@
-# from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+TEST_USER_EMAIL = "iama@test.com"
+TEST_USER_PASSWORD = "mytemporarypassword"
+
+
+class IndexViewTest(TestCase):
+    def test_force_authentication(self):
+        client = Client()
+        response = client.get(reverse("shifter_files:index"))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(reverse("shifter_auth:login") in response.url)
