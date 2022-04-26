6924a6a4ee34d492d113898bc32c7dc7cb837a36
Toby Such <toby@tobysuch.uk>
2022-04-26 13:13:24 +0100

    Add test_models.py

---
+++ shifter/shifter_files/tests/test_models.py
@@ -0,0 +1,45 @@
+import datetime
+import pathlib
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from shifter_files.models import FileUpload
+
+
+TEST_USER_EMAIL = "iama@test.com"
+TEST_USER_PASSWORD = "mytemporarypassword"
+
+TEST_FILE_NAME = "mytestfile.txt"
+TEST_FILE_CONTENT = b"Hello, World!"
+
+
+class FileUploadModelTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(TEST_USER_EMAIL,
+                                             TEST_USER_PASSWORD)
+
+    def test_add_new_file(self):
+        test_file = SimpleUploadedFile(TEST_FILE_NAME, TEST_FILE_CONTENT)
+        current_datetime = timezone.now()
+        expiry_datetime = current_datetime + datetime.timedelta(days=1)
+        file_upload = FileUpload.objects.create(
+            owner=self.user, file_content=test_file,
+            upload_datetime=current_datetime,
+            expiry_datetime=expiry_datetime,
+            filename=TEST_FILE_NAME)
+
+        self.assertEqual(FileUpload.objects.count(), 1)
+        file_upload = FileUpload.objects.first()
+        self.assertEqual(file_upload.filename, TEST_FILE_NAME)
+        self.assertEqual(file_upload.owner, self.user)
+        self.assertAlmostEqual(file_upload.upload_datetime, current_datetime,
+                               delta=datetime.timedelta(minutes=1))
+        self.assertAlmostEqual(file_upload.expiry_datetime, expiry_datetime,
+                               delta=datetime.timedelta(minutes=1))
+        # Ensure file has been uploaded to the correct location
+        path = pathlib.Path("media/uploads/" + TEST_FILE_NAME)
+        self.assertTrue(path.is_file())
