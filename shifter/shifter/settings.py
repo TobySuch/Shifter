bdbebeccc3b5a4a5207e1d690838946cb23232ff
Toby Such <toby@tobysuch.uk>
2022-04-26 13:45:41 +0100

    Add FileDownloadView

--- shifter/shifter/settings.py
+++ shifter/shifter/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
