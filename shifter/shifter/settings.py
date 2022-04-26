6f8b52d1504a638588e63a8391f471451804d642
Toby Such <toby@tobysuch.uk>
2022-04-26 15:35:07 +0100

    Add logging config

--- shifter/shifter/settings.py
+++ shifter/shifter/settings.py
@@ -123,6 +123,25 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'handlers': {
+      'file': {
+         'level': 'DEBUG',
+         'class': 'logging.FileHandler',
+         'filename': '/tmp/debug.log',
+      },
+   },
+   'loggers': {
+      'django': {
+         'handlers': ['file'],
+         'level': 'DEBUG',
+         'propagate': True,
+      },
+   },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
