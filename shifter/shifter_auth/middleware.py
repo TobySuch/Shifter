9a2bd6e4453ec8260e2ced8726ca2dba2a5d4fce
Toby Such <toby@tobysuch.uk>
2022-10-09 00:31:32 +0100

    Add allow list for paths to skip change password enforcement

--- shifter/shifter_auth/middleware.py
+++ shifter/shifter_auth/middleware.py
@@ -5,10 +5,14 @@ from django.urls import reverse
 def ensure_password_changed(get_response):
     CHANGE_PASSWORD_URL = "shifter_auth:change-password"
 
+    ALLOW_LIST = [
+        reverse("shifter_auth:logout")
+    ] + [reverse(CHANGE_PASSWORD_URL)]
+
     def middleware(request):
         if not request.user.is_anonymous:
             if (request.user.change_password_on_login
-                    and request.path != reverse(CHANGE_PASSWORD_URL)):
+                    and request.path not in ALLOW_LIST):
                 return redirect(CHANGE_PASSWORD_URL)
 
         response = get_response(request)
