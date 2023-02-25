d34432c3099581415299e858b37072c1276ffed9
Toby Such <toby@tobysuch.uk>
2023-02-25 02:14:51 +0000

    Add client side zipping for multiple files

--- shifter/shifter_files/static/shifter_files/js/filepond_controller.js
+++ shifter/shifter_files/static/shifter_files/js/filepond_controller.js
@@ -1,9 +1,38 @@
+function combineFiles(pond) {
+    let files = pond.getFiles();
+
+    // If there is more than one file, zip them into one file
+    if (files.length > 1) {
+        console.debug("Combining files")
+        
+        const zip = new JSZip();
+        const promises = [];
+        files.forEach((file) => {
+            const promise = new Promise((resolve, reject) => {
+                zip.file(file.filename, file.file, {binary: true});
+                resolve();
+            });
+            promises.push(promise);
+        });
+
+        return Promise.all(promises)
+            .then(async () => {
+                await pond.removeFiles()
+                let blob = await zip.generateAsync({type: "blob"})
+                await pond.addFile(new File([blob], 'combined.zip', { type: 'application/zip' }));
+            })
+    }
+
+    // If there is only one file, do nothing
+    return Promise.resolve();
+}
+
 function setupFilepond(filepondElementName, expiryDatetimeElementName) {
     const inputElement = document.getElementsByName(filepondElementName)[0];
 
     const pond = FilePond.create(inputElement, {
         name: filepondElementName,
-        allowMultiple: false,
+        allowMultiple: true,
         allowProcess: false,
         allowRevert: false,
         credits: false,
@@ -16,15 +45,13 @@ function setupFilepond(filepondElementName, expiryDatetimeElementName) {
                     "X-CSRFToken": document.querySelector('input[name="csrfmiddlewaretoken"]').value
                 },
                 timeout: 300 * 1000, // 5 minutes
-                onload: null,
-                onerror: null,
                 ondata: (formData) => {
                     // Add the rest of the form data
                     formData.append("csrfmiddlewaretoken", document.querySelector('input[name="csrfmiddlewaretoken"]').value);
                     formData.append("expiry_datetime", document.querySelector('input[name="' + expiryDatetimeElementName + '"]').value);
                     return formData;
                 },
-                onload(response) {
+                onload: (response) => {
                     redirectUrl = JSON.parse(response).redirect_url;
                     window.location.href = redirectUrl;
                 },
@@ -47,6 +74,9 @@ function setupFilepond(filepondElementName, expiryDatetimeElementName) {
     const uploadButton = document.getElementById('upload-btn');
 
     uploadButton.addEventListener('click', () => {
-        pond.processFiles();
+        combineFiles(pond).then(() => {
+            console.debug("Uploading file")
+            pond.processFiles();
+        });
     });
 }
