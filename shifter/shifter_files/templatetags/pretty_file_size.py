4d12c57aa469047b1edac3dcc071975f320f8dda
Toby Such <toby@tobysuch.uk>
2023-06-23 21:51:02 +0100

    Use factor 1000 on file size calculation

--- shifter/shifter_files/templatetags/pretty_file_size.py
+++ shifter/shifter_files/templatetags/pretty_file_size.py
@@ -6,17 +6,17 @@ register = template.Library()
 def pretty_file_size(value):
     """Converts a file size in bytes to a human readable format"""
     bytes = int(value)
-    kilobytes = bytes // 1024
-    megabytes = kilobytes // 1024
-    gigabytes = megabytes // 1024
-    terabytes = gigabytes // 1024
-    if bytes < 1024:
+    kilobytes = bytes // 1000
+    megabytes = kilobytes // 1000
+    gigabytes = megabytes // 1000
+    terabytes = gigabytes // 1000
+    if bytes < 1000:
         return f"{bytes}B"
-    elif kilobytes < 1024:
+    elif kilobytes < 1000:
         return f"{kilobytes}KB"
-    elif megabytes < 1024:
+    elif megabytes < 1000:
         return f"{megabytes}MB"
-    elif gigabytes < 1024:
+    elif gigabytes < 1000:
         return f"{gigabytes}GB"
     else:
         return f"{terabytes}TB"
--- shifter/shifter_files/tests/test_templatetags.py
+++ shifter/shifter_files/tests/test_templatetags.py
@@ -2,32 +2,32 @@ from django.test import TestCase
 
 from shifter_files.templatetags.pretty_file_size import pretty_file_size
 
-kilobyte = 1024
-megabyte = kilobyte * 1024
-gigabyte = megabyte * 1024
-terabyte = gigabyte * 1024
-petabyte = terabyte * 1024
+kilobyte = 1000
+megabyte = kilobyte * 1000
+gigabyte = megabyte * 1000
+terabyte = gigabyte * 1000
+petabyte = terabyte * 1000
 
 
 class PrettyFileSizeTest(TestCase):
     def test_bytes(self):
         self.assertEqual(pretty_file_size(1), "1B")
-        self.assertEqual(pretty_file_size(1023), "1023B")
+        self.assertEqual(pretty_file_size(999), "999B")
 
     def test_kilobytes(self):
         self.assertEqual(pretty_file_size(kilobyte), "1KB")
-        self.assertEqual(pretty_file_size(megabyte - 1), "1023KB")
+        self.assertEqual(pretty_file_size(megabyte - 1), "999KB")
 
     def test_megabytes(self):
         self.assertEqual(pretty_file_size(megabyte), "1MB")
-        self.assertEqual(pretty_file_size(gigabyte - 1), "1023MB")
+        self.assertEqual(pretty_file_size(gigabyte - 1), "999MB")
 
     def test_gigabytes(self):
         self.assertEqual(pretty_file_size(gigabyte), "1GB")
         self.assertEqual(pretty_file_size(terabyte - 1),
-                         "1023GB")
+                         "999GB")
 
     def test_terabytes(self):
         self.assertEqual(pretty_file_size(terabyte), "1TB")
-        self.assertEqual(pretty_file_size(petabyte - 1), "1023TB")
-        self.assertEqual(pretty_file_size(petabyte), "1024TB")
+        self.assertEqual(pretty_file_size(petabyte - 1), "999TB")
+        self.assertEqual(pretty_file_size(petabyte), "1000TB")
