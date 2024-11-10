9dbf5a9f7e1c715a7ccdfa2bcccc071dd5b139de
Toby Such <toby@tobysuch.uk>
2024-11-10 14:36:02 +0000

    Add first time setup page

--- shifter/shifter_auth/views.py
+++ shifter/shifter_auth/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import get_user_model, logout
+from django.contrib.auth import get_user_model, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect
@@ -8,6 +8,7 @@ from django.views.decorators.http import require_POST
 from django.views.generic.edit import FormView
 
 from .forms import ChangePasswordForm, NewUserForm
+from .middleware import is_first_time_setup_required
 
 
 @require_POST
@@ -54,5 +55,38 @@ class CreateNewUserView(UserPassesTestMixin, FormView):
         messages.add_message(
             self.request, messages.INFO, "User successfully created."
         )
+
+        return super().form_valid(form)
+
+
+class FirstTimeSetupView(UserPassesTestMixin, FormView):
+    template_name = "shifter_auth/setup.html"
+    form_class = NewUserForm
+    success_url = reverse_lazy("shifter_files:index")
+    permission_denied_message = "First time setup has already been completed."
+
+    def test_func(self):
+        return is_first_time_setup_required()
+
+    def form_valid(self, form):
+        User = get_user_model()
+        email = form.cleaned_data["email"]
+        password = form.cleaned_data["password"]
+        user = User.objects.create_user(email, password)
+        user.change_password_on_login = False
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        messages.add_message(
+            self.request,
+            messages.INFO,
+            (
+                "Here you can upload one or multiple files. Once the files "
+                "have been uploaded, you will be given a link to share for "
+                "others to download."
+            ),
+        )
+
+        login(self.request, user)
 
         return super().form_valid(form)
