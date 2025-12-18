292fb229fc0d9694e765195f2783c9bee9b11e86
Toby Such <toby@tobysuch.uk>
2025-12-18 21:39:02 +0000

    build: install alpinejs

--- shifter/assets/js/shifter.js
+++ shifter/assets/js/shifter.js
@@ -2,6 +2,10 @@
 
 import "../css/style.css";
 import "flowbite";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+Alpine.start();
 
 let dtFormat = new Intl.DateTimeFormat(undefined, {
   year: "numeric",
